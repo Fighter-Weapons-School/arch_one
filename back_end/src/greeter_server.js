441d12c1db0c7836079c1b5c5e67f2d6742e3d45
scott sinclair <scottcsinclair@gmail.com>
2022-09-29 16:59:26 -0400

    Update to add linting Update to add environment vars

--- back_end/src/greeter_server.js
+++ back_end/src/greeter_server.js
@@ -1,33 +1,54 @@
+// TODO: call back a function rather than inline.
+// TODO: Add logging and metrics collection.
+// TODO: Add light error handling
+// TODO: Add linting (configure linting to fix end of line and line length rules)
+// TODO: Tests to ensure environment vars are only used in development
+// TODO: Tests to ensure environment vars are set
+// TODO: Tests to ensure environment vars can be overridden
 
-var PROTO_PATH = __dirname + '/../../protos/greeter.proto';
+const PROTO_PATH = __dirname + '/../../protos/greeter.proto';
 
-var grpc = require('@grpc/grpc-js');
-var protoLoader = require('@grpc/proto-loader');
-var packageDefinition = protoLoader.loadSync(
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+// grpc boilerplate code
+// dynamic creation the services defined in the proto file
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
+    {
+      keepCase: true,
+      longs: String,
+      enums: String,
+      defaults: true,
+      oneofs: true
     });
-var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+// maps to the proto file package definition
+const helloProto = grpc.loadPackageDefinition(packageDefinition).helloworld;
 
 /**
- * Implements the SayHello RPC method.
+ * Implementation of the proto 'SayHello' method
+ * @param {*} call 
+ * @param {*} callback 
  */
 function sayHello(call, callback) {
   callback(null, {message: 'Hello ' + call.request.name});
 }
 
 /**
- * Starts an RPC server that receives requests for the Greeter service at the
- * sample server port
+ * Starts an RPC server that receives requests for the Greeter service at the sample server port
  */
 function main() {
-  var server = new grpc.Server();
-  server.addService(hello_proto.Greeter.service, {sayHello: sayHello});
-  server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+  const server = new grpc.Server();
+  const testIt = `${process.env.GREETER_IP}:${process.env.GREETER_PORT}`;
+  console.log(testIt);
+  
+  // maps to the proto file service definition
+  server.addService(helloProto.Greeter.service, {sayHello: sayHello});
+  server.bindAsync(testIt, grpc.ServerCredentials.createInsecure(), () => {
     server.start();
   });
 }
